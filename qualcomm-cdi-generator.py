194bc0e4d912ffe7cd286e57a6849cf9d69544ef
Koen Kooi <koen.kooi@oss.qualcomm.com>
2025-09-24 15:51:25 +0200

    qualcomm-cdi-generator.py: add support for catchall entries
    The 'render:all' and 'video:all' aliases contain all detected nodes of
    that sort.
    
    Also add TODO entries.
    
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- qualcomm-cdi-generator.py
+++ qualcomm-cdi-generator.py
@@ -14,7 +14,7 @@ def find_devicenodes(deviceglob):
 
 def generate_devicenodes_cdi(nickname, filesglob):
     rendernodeindex = 0
-    rendernodelist = [None] * len(filesglob)
+    rendernodelist = [None] * (len(filesglob) + 1)
     for rendernode in sorted(filesglob):
         gpu_path = {"path": rendernode} 
         gpu_pathlist = { "deviceNodes": [ gpu_path ] }
@@ -22,11 +22,24 @@ def generate_devicenodes_cdi(nickname, filesglob):
         print(device_gpu, file=sys.stderr)
         rendernodelist[rendernodeindex] = device_gpu
         rendernodeindex += 1
+
+    catchallindex = 0
+    gpu_paths = [None] * len(filesglob)
+    for rendernode in sorted(filesglob):
+        gpu_paths[catchallindex] = {"path": rendernode}
+        catchallindex += 1
+    gpu_pathlist = { "deviceNodes":  gpu_paths  }
+    device_gpus = { "name": nickname+":all", "containerEdits": gpu_pathlist }
+    rendernodelist[rendernodeindex] = device_gpus
+
     return rendernodelist
 
+# Find rendernodes and create entries for them
 rendernodes = find_devicenodes('/dev/dri/render*')
 render_cdi = generate_devicenodes_cdi('render', rendernodes)
 
+# Find all videonoodes and generate entries
+# TODO: add input/output filters to make selecting between encoders, decoders and cameras easier
 videonodes = find_devicenodes('/dev/video*')
 video_cdi = generate_devicenodes_cdi('video', videonodes)
 
