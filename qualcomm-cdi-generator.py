c01a9c38cbe36c71bad0b534a1230399df3eec97
Koen Kooi <koen.kooi@oss.qualcomm.com>
2025-09-26 10:54:29 +0200

    qualcomm-cdi-generator.py: don't output json for missing devices
    When one or more devices or groups aren't detected, don't output any
    json.
    
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- qualcomm-cdi-generator.py
+++ qualcomm-cdi-generator.py
@@ -20,25 +20,27 @@ def find_devicenodes(deviceglob):
     return files
 
 def generate_devicenodes_cdi(nickname, filesglob):
-    rendernodeindex = 0
-    rendernodelist = [None] * (len(filesglob) + 1)
-    for rendernode in sorted(filesglob):
-        gpu_path = {"path": rendernode} 
-        gpu_pathlist = { "deviceNodes": [ gpu_path ] }
-        device_gpu = { "name": nickname+str(rendernodeindex), "containerEdits": gpu_pathlist }
-        print(device_gpu, file=sys.stderr)
-        rendernodelist[rendernodeindex] = device_gpu
-        rendernodeindex += 1
+    if filesglob:
+        rendernodeindex = 0
+        rendernodelist = [None] * (len(filesglob) + 1)
+        for rendernode in sorted(filesglob):
+            gpu_path = {"path": rendernode} 
+            gpu_pathlist = { "deviceNodes": [ gpu_path ] }
+            device_gpu = { "name": nickname+str(rendernodeindex), "containerEdits": gpu_pathlist }
+            print(device_gpu, file=sys.stderr)
+            rendernodelist[rendernodeindex] = device_gpu
+            rendernodeindex += 1
 
-    catchallindex = 0
-    gpu_paths = [None] * len(filesglob)
-    for rendernode in sorted(filesglob):
-        gpu_paths[catchallindex] = {"path": rendernode}
-        catchallindex += 1
-    gpu_pathlist = { "deviceNodes":  gpu_paths  }
-    device_gpus = { "name": nickname+":all", "containerEdits": gpu_pathlist }
-    rendernodelist[rendernodeindex] = device_gpus
-
+        catchallindex = 0
+        gpu_paths = [None] * len(filesglob)
+        for rendernode in sorted(filesglob):
+            gpu_paths[catchallindex] = {"path": rendernode}
+            catchallindex += 1
+        gpu_pathlist = { "deviceNodes":  gpu_paths  }
+        device_gpus = { "name": nickname+":all", "containerEdits": gpu_pathlist }
+        rendernodelist[rendernodeindex] = device_gpus
+    else:
+        rendernodelist = []
     return rendernodelist
 
 # Find rendernodes and create entries for them
