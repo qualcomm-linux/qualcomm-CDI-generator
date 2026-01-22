b4e3721d4712bcb3b073fa1000c1e29d458fe4cd
Koen Kooi <koen.kooi@oss.qualcomm.com>
2026-01-22 13:56:41 +0100

    qualcomm-cdi-generator.py partially fix CDI spec violation
    The previous commit introduced this error:
    
    ```
    CDI: Error associated with spec file /var/run/cdi/qualcomm.json: failed to load CDI Spec failed to parse CDI Spec "/var/run/cdi/qualcomm.json": failed to unmarshal CDI Spec: error unmarshaling JSON: while decoding JSON: json: cannot unmarshal array into Go struct field ContainerEdits.devices.containerEdits.deviceNodes of type specs.DeviceNode
    ```
    
    This is caused by python-json adding an extra set of [], which violates
    the CDI spec. Fix that for the cdsp entry at the cost of removing the
    -secure variant from the :all entry.
    
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- qualcomm-cdi-generator.py
+++ qualcomm-cdi-generator.py
@@ -43,13 +43,14 @@ def generate_devicenodes_cdi(nickname, filesglob):
         devicenodeindex = 0
         devicenodelist = [None] * (len(filesglob) + 1)
         for devicenode in sorted(filesglob):
+            device_path = {"path": devicenode}
             # Special case cdsp, which was a -secure sibling node
             if str(devicenode).endswith('cdsp'):
                 logging.debug("CDSP detected, adding regular and -secure variants")
-                device_path = {"path": devicenode}, {"path": devicenode + "-secure"}
+                securedevice_path = {"path": devicenode + "-secure"}
+                device_pathlist = { "deviceNodes": [ device_path, securedevice_path ] }
             else:
-                device_path = {"path": devicenode}
-            device_pathlist = { "deviceNodes": [ device_path ] }
+                device_pathlist = { "deviceNodes": [ device_path ] }
             cdi_index = get_devicenode_index(devicenode)
             # If there's only one match *and* it doesn't have its own index, don't add the '0' index
             if len(filesglob) == 1 and not cdi_index:
@@ -66,11 +67,7 @@ def generate_devicenodes_cdi(nickname, filesglob):
         catchallindex = 0
         device_paths = [None] * len(filesglob)
         for devicenode in sorted(filesglob):
-            if str(devicenode).endswith('cdsp'):
-                logging.debug("CDSP detected, adding regular and -secure variants")
-                device_paths[catchallindex] = {"path": devicenode}, {"path": devicenode + "-secure"}
-            else:
-                device_paths[catchallindex] = {"path": devicenode}
+            device_paths[catchallindex] = {"path": devicenode}
             catchallindex += 1
         device_pathlist = { "deviceNodes":  device_paths  }
         device_entrys = { "name": nickname+":all", "containerEdits": device_pathlist }
